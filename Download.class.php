853e8c4758d940f96d4fbcaa0dd915d2a1d6255d
doner <cefar@mail.ru>
2017-10-08 03:37:26 +0500

    s

--- Download.class.php
+++ Download.class.php
@@ -13,6 +13,8 @@
 
 class Download {
 
+
+
 	public static function wget( $from_file, $to_file ) {
 		if ( Os :: is_windows( ) ) $stdout = ' 2>&1'; // > NUL
 		else $stdout = ' 2>&1'; // > /dev/null
@@ -20,8 +22,11 @@ class Download {
 		return $download_strout;
 	}
 
+
 	public static function curl( $from_file, $to_file ) {
+
 		$stat_url = @get_headers( $from_file, 1 );
+
 		// Что то не так
 		if ( !$stat_url || empty( $stat_url[ 0 ] ) || $stat_url[ 0 ] != 'HTTP/1.1 200 OK' ) {
 			$stat_url[ 0 ] = ( isset( $stat_url[ 0 ] ) ) ? $stat_url[ 0 ] : 'null';
@@ -44,8 +49,7 @@ class Download {
 					return true;
 				}
 			}
-			$fp = fopen( $to_file, "w" );
-			$options[ CURLOPT_FILE ] = $fp;
+			$options[ CURLOPT_FILE ] = fopen( $to_file, 'x' );
 		}
 		else {
 			$options[ CURLOPT_RETURNTRANSFER ] = true;
@@ -54,6 +58,7 @@ class Download {
 		curl_setopt_array( $ch, $options );
 		$result = curl_exec( $ch );
 		$info = curl_getinfo( $ch );
+
 		curl_close( $ch );
 
 		if ( $info[ 'http_code' ] == '200' ) {
@@ -63,9 +68,77 @@ class Download {
 			Registry :: __instance( ) -> logging -> error( "http_code " . $info[ 'http_code' ] . "\t" . $from_file );
 			return false;
 		}
+
 	}
 
 
+
+	public static function multicurl( $from_files = array( ), $to_files = array( ) ) {
+
+	  $curly = array( );
+	  $info = array( );
+	  $ret = array( );
+
+	  $mh = curl_multi_init( );
+
+	  foreach ( $from_files as $id => $f ) {
+
+		$curly[ $id ] = curl_init( $f );
+		curl_setopt( $curly[ $id ], CURLOPT_HEADER, 0 );
+		curl_setopt( $curly[ $id ], CURLINFO_HEADER_OUT, false );
+		curl_setopt( $curly[ $id ], CURLOPT_TIMEOUT, 28800 );
+		curl_setopt( $curly[ $id ], CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $curly[ $id ], CURLOPT_SSL_VERIFYHOST, false );
+
+		if ( file_exists( $to_files[ $id ] ) ) {
+			$ret[ $id ] = "download_file_exist\t" . $to_files[ $id ];
+			Registry :: __instance( ) -> logging -> debug( $ret[ $id ] );
+			continue;
+		}
+
+		curl_setopt( $curly[ $id ], CURLOPT_FILE, fopen( $to_files[ $id ], 'x' ) );
+		curl_multi_add_handle( $mh, $curly[ $id ] );
+	  }
+
+	  // execute the handles
+	  $running = null;
+	  do {
+		curl_multi_exec( $mh, $running );
+	  } while( $running > 0 );
+
+
+	  // get content and remove handles
+	  foreach( $curly as $id => $c ) {
+		$info[ $id ] = curl_getinfo( $c );
+		curl_multi_remove_handle( $mh, $c );
+	  }
+
+	  // all done
+	  curl_multi_close( $mh );
+
+	  foreach( $info as $id => $i ) {
+		if ( $i[ 'http_code' ] == '200' ) {
+			$ret[ $id ] = date( 'Y-m-d h:i:s' ) . ' URL:' . $from_files[ $id ] . ' [' . $i[ "download_content_length" ] . '/' . $i[ "size_download" ] . '] -> "' . $to_files[ $id ] . '" [1]';
+		}
+		else {
+			$ret[ $id ] = "download_code\t" . $i[ 'http_code' ] . "\t" . $from_files[ $id ];
+			Registry :: __instance( ) -> logging -> error( $ret[ $id ] );
+		}
+	  }
+
+	  return $ret;
+
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 	public static function smtp( $from = array( ), $to = '/', $connect = array( ) ) {
 		return false;
